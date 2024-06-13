34dba0a6ad70dba95475a061bae620a57137727e
Jaro-23 <robbejaeken23@gmail.com>
2024-06-13 19:29:38 +0200

    Fixed lsp

--- lua/jaroNvim/packer.lua
+++ lua/jaroNvim/packer.lua
@@ -1,16 +1,30 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use( 'mbbill/undotree' )
-  use( 'tpope/vim-fugitive' )
-  use { "catppuccin/nvim", as = "catppuccin" }
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			{'neovim/nvim-lspconfig'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+
+	use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use( 'mbbill/undotree' )
+	use( 'tpope/vim-fugitive' )
+	use { "catppuccin/nvim", as = "catppuccin" }
 end)
