da76b5e011543db6fa2e961f771d4051c645d11d
Jaro-23 <robbejaeken23@gmail.com>
2024-06-13 16:59:32 +0200

    Added fugitie

--- lua/jaroNvim/packer.lua
+++ lua/jaroNvim/packer.lua
@@ -10,5 +10,7 @@ return require('packer').startup(function(use)
   }
 
   use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
+  use( 'mbbill/undotree' )
+  use( 'tpope/vim-fugitive' )
+  use { "catppuccin/nvim", as = "catppuccin" }
 end)
