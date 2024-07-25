1091f4041b8c590487b31f191470a1959bf6033e
Jaro-23 <robbejaeken23@gmail.com>
2024-07-25 16:33:19 +0200

    Updates

--- lua/jaroNvim/init.lua
+++ lua/jaroNvim/init.lua
@@ -1,1 +1,36 @@
 require("jaroNvim.remap")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.mouse = 'a'
+vim.opt.showmode = true
+
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+vim.opt.hlsearch = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = '',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function ()
+		vim.highlight.on_yank()
+	end,
+})
